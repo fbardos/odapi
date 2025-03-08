7d10133354016d5c8d55cd3d38b4e5963056be77
Fabian <fbardos@proton.me>
2025-03-08 09:04:05 +0100

    Fixes for INTM tests
    - added prefix odapi_ for global tests for better identification
    - fixed failing intm tests after rerunning all assets

--- models/tests/generic/intm.sql
+++ models/tests/generic/intm.sql
@@ -1,6 +1,6 @@
 -- Copy of dbt_expectations.expect_table_columns_to_match_ordered_list,
 -- because - unlike macros - tests cannot be referenced in other tests.
-{% test intm_columns(model, transform='upper') %}
+{% test odapi_intm_columns(model, transform='upper') %}
 {% set column_list = [
     'indicator_id',
     'geo_code',
@@ -19,8 +19,19 @@
 {% endtest %}
 
 
+--------------------------------------------------------------------------------
+-- GLOBAL PK
+--------------------------------------------------------------------------------
+{% test odapi_intm_pk(model) %}
+    {% set combination_of_columns = ['indicator_id', 'geo_code', 'geo_value', 'period_ref', 'knowledge_date_from'] %}
+    {{ return(adapter.dispatch('test_unique_combination_of_columns', 'dbt_utils')(model, combination_of_columns, quote_columns=False)) }}
+{% endtest %}
+
+
+--------------------------------------------------------------------------------
 -- expect at least 2000 municipalities in tables with municipalities
-{% test intm_unique_municipalities(model) %}
+--------------------------------------------------------------------------------
+{% test odapi_intm_unique_municipalities(model) %}
     {{ adapter.dispatch('test_expect_table_row_count_to_be_between', 'dbt_expectations') (
         model,
         min_value=2000,
@@ -32,25 +43,16 @@
 {% endtest %}
 
 
--- expect 2000 or more municipalities
-{% test intm_unique_municipalities(model) %}
-    {{ adapter.dispatch('test_expect_table_row_count_to_be_between', 'dbt_expectations') (
-        model,
-        min_value=2000,
-        max_value=None,
-        group_by=['knowledge_date_from', 'period_ref'],
-        row_condition="geo_code = 'polg'",
-        strictly=False,
-    ) }}
-{% endtest %}
-
-
+--------------------------------------------------------------------------------
 -- expect at least one row
-{% test intm_rowsmin(model) %}
+--------------------------------------------------------------------------------
+{% test odapi_intm_rowsmin(model) %}
 	{{ adapter.dispatch('test_expect_table_row_count_to_be_between', 'dbt_expectations') (
+        model,
         min_value=1,
         max_value=None,
         group_by=None,
         row_condition=None,
         strictly=False,
 ) }} 
+{% endtest %}
