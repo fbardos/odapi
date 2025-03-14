716b06eb22899d394db7a0a88db88f866bb90418
Fabian <fbardos@proton.me>
2025-03-14 23:19:42 +0100

    Upgraded to dagster 1.10.5
    Used to make use of pools in dagster. Additionally, removed class
    constants in ConfigurableResource subclass.

--- dag/odapi/assets/dbt.py
+++ dag/odapi/assets/dbt.py
@@ -1,11 +1,12 @@
 import os
 from pathlib import Path
+
 from dagster import AssetExecutionContext
+from dagster import get_dagster_logger
 from dagster_dbt import DbtCliResource
 from dagster_dbt import DbtProject
 from dagster_dbt import dbt_assets
 from dotenv import load_dotenv
-from dagster import get_dagster_logger
 
 ################################################################################
 # DBT
@@ -13,11 +14,14 @@ from dagster import get_dagster_logger
 load_dotenv()
 
 dbt_path_str = os.getenv('DBT__ABSOLUTE_PATH')
+dbt_profiles_str = os.getenv('DBT__PROFILES_PATH')
 assert dbt_path_str is not None
-dbt_path = Path(dbt_path_str).resolve()
-dbt_project = DbtProject(project_dir=dbt_path)
+assert dbt_profiles_str is not None
+dbt_path = Path(dbt_path_str).absolute()
+dbt_profiles = Path(dbt_profiles_str).resolve()
+dbt_project = DbtProject(project_dir=dbt_path, profiles_dir=dbt_profiles)
 
-dbt = DbtCliResource(project_dir=dbt_project)
+dbt = DbtCliResource(project_dir=dbt_project, profiles_dir=dbt_profiles)
 
 if os.getenv('DAGSTER_DBT_PARSE_PROJECT_ON_LOAD'):
     logger = get_dagster_logger()
