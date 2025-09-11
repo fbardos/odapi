3bda8be59da3297c0034eaec61407964dd2f1295
Fabian Bardos <fabian@bardos.dev>
2025-09-11 23:20:23 +0200

    Added measure_code to API /indicators

--- models/models/marts/mart_available_indicator.sql
+++ models/models/marts/mart_available_indicator.sql
@@ -1,4 +1,4 @@
-select distinct
+select
     api.geo_code
     , api.indicator_id
     , indi.indicator_name
@@ -8,8 +8,19 @@ select distinct
     , indi.topic_4
     , indi.indicator_unit
     , indi.indicator_description
+    , array_agg(distinct api.measure_code) as measure_codes
 from {{ ref('full_mart_ogd_api') }} api
     join {{ ref('seed_indicator') }} indi on api.indicator_id = indi.indicator_id
+group by
+    api.indicator_id
+    , api.geo_code
+    , indi.indicator_name
+    , indi.topic_1
+    , indi.topic_2
+    , indi.topic_3
+    , indi.topic_4
+    , indi.indicator_unit
+    , indi.indicator_description
 order by api.indicator_id
 
 
