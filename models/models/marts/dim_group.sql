06f446b6e49953afbac02273f4321f6161841751
Fabian Bardos <fabian@bardos.dev>
2025-10-03 12:09:54 +0200

    Fixed duplicate keys on unique values
    Could be debugged later, why duplicate values even happen.

--- models/models/marts/dim_group.sql
+++ models/models/marts/dim_group.sql
@@ -1,2 +1,3 @@
-select *
+select distinct on (group_name) *
 from {{ source('py_intermediate', 'intm_meta_group') }}
+order by group_name, group_id
--- models/models/marts/dim_group_value.sql
+++ models/models/marts/dim_group_value.sql
@@ -1,2 +1,3 @@
-select *
+select distinct on (group_value_name) *
 from {{ source('py_intermediate', 'intm_meta_group_value') }}
+order by group_value_name, group_value_id
