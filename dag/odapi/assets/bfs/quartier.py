d8afeb3c7a79b8053bf830d6f859429fb5cd887e
Fabian Bardos <fabian@bardos.dev>
2026-08-08 00:20:10 +0200

    fix(dag): bfs_quartier, switch to duckdb
    Switched old reference to postgres for job_bfs_quartier to duckdb.

--- dag/odapi/assets/bfs/quartier.py
+++ dag/odapi/assets/bfs/quartier.py
@@ -2,6 +2,7 @@ from dagster import AssetExecutionContext
 from dagster import asset
 from dagster import define_asset_job
 
+from odapi.resources.duckdb.duckdb import DuckDBResource
 from odapi.resources.postgres.postgres import PostgresResource
 from odapi.resources.url.geojson import QuartierBoundaries
 
@@ -14,15 +15,26 @@ from odapi.resources.url.geojson import QuartierBoundaries
 def bfs_quartier(
     context: AssetExecutionContext,
     geo_quartier: QuartierBoundaries,
-    db: PostgresResource,
+    duckdb: DuckDBResource,
 ):
     gdf = geo_quartier.load_from_zip('ag-b-00.03-95-qg24/json/quart24.geojson')
-    gdf.to_postgis(
-        name='bfs_quartier',
-        con=db.get_sqlalchemy_engine(),
-        schema='src',
-        if_exists='replace',
-    )
+    engine = duckdb.get_sqlalchemy_engine()
+    with engine.connect() as con:
+        duckdb_con = con.connection.driver_connection  # need of the duckdb driver...
+        assert duckdb_con
+        duckdb_con.execute('INSTALL spatial')
+        duckdb_con.execute('INSTALL spatial')
+        df = gdf.copy()
+        df['geometry'] = df.geometry.to_wkb()
+        duckdb_con.register('gdf', df)
+        duckdb_con.execute('''
+            CREATE OR REPLACE TABLE src.bfs_quartier AS
+            SELECT
+                * EXCLUDE geometry,
+                ST_GeomFromWKB(geometry) AS geometry
+            FROM gdf
+        ''')
+        duckdb_con.unregister('gdf')
 
 
 job = define_asset_job(
